4876824ae225b9a917f926e63a6c62cfbe269c92
Marika Wei <dev@miphol.com>
2016-08-24 13:58:33 +0800

    Fix Elixir module
    Almost one-to-one translation from Erlang

--- src/complex.ex
+++ src/complex.ex
@@ -1,14 +1,13 @@
 defmodule Complex do
-
-  @node :"c1@NEWBORN"
-
   def foo(x), do:
     call_cnode {:foo, x}
   def bar(y), do:
     call_cnode {:bar, y}
 
   defp call_cnode(msg) do
-    send {:any, @node}, {:call, self, msg}
+    {:ok, hostname} = :inet.gethostname()
+    hostname = to_string hostname
+    send {:any, String.to_atom "c1@" <> hostname}, {:call, self, msg}
     receive do
       {:cnode, result} -> result
     end
